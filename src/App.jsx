3e42b6dd7bada360af3c5c97aff0924057038e23
shahinexy <shahinexy@gmail.com>
2024-12-15 18:53:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <div className="w-full h-[100vh]">
+      <div className="w-full h-[100vh] bg-green-500 flex justify-center items-center">
+        <div className="flex">
+          <button className="bg-white px-2 border border-gray-500">V</button>
+          <button className="bg-white px-2 border border-gray-500">H</button>
+          <button className="bg-white px-2 border border-gray-500">-</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
